c75603bec16ff7451c6d4df52f6e9e233846f5d3
seo-rii <me@seorii.page>
2024-06-18 05:56:37 +0900

    feat: add google stt for better result

--- src/components/global/Speak.tsx
+++ src/components/global/Speak.tsx
@@ -19,6 +19,7 @@ function WaveForm() {
 export default function Speak() {
     const dispatch = useDispatch();
     const [recording, setRecording] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [message, setMessage] = useState("");
     const [stop, setStop] = useState(null as any);
     const messages = useSelector((state: any) => state.chat.messages);
@@ -26,9 +27,10 @@ export default function Speak() {
     const record = () => {
         setRecording(true);
         const pr = stt(setMessage);
-        setStop(() => () => {
-            pr.stop();
-            setRecording(false);
+        setStop(() => (v: any) => {
+            pr.stop(v);
+            if (v) setRecording(false);
+            return pr;
         });
         pr.then((result) => {
             setRecording(false);
@@ -41,18 +43,26 @@ export default function Speak() {
 
     return <div
         className={`p-4 w-full flex flex-col items-center justify-center ${recording ? 'h-52' : 'h-32'}`}>
-        {recording ? <>
+        {recording ? (loading ? <>
+            <p className="flex-1 flex items-center justify-center">{message}</p>
+            <WaveForm/>
+            <p className="flex-1 flex items-center justify-center">음성을 인식하고 있어요...</p>
+        </> : <>
             <p className="flex-1 flex items-center justify-center">{message}</p>
             <WaveForm/>
             <div className="flex w-full max-w-96">
-                <Button className="flex-2 mx-1" outlined onClick={stop}>취소</Button>
+                <Button className="flex-2 mx-1" outlined onClick={() => stop(true)}>취소</Button>
                 <Button className="flex-1 mx-1" onClick={() => {
-                    stop();
-                    gpt([...messages, {role: 'user', content: message}], dispatch);
-                    dispatch(addMessage(message));
+                    setLoading(true);
+                    stop().then((message: string) => {
+                        setRecording(false);
+                        setLoading(false);
+                        gpt([...messages, {role: 'user', content: message}], dispatch);
+                        dispatch(addMessage(message));
+                    });
                 }}>답변 완료</Button>
             </div>
-        </> : <>
+        </>) : <>
             <p className="text-blue-500 text-xs">마이크를 눌러 녹음을 진행해주세요.</p>
             <button
                 className="bg-blue-500 text-white rounded-full mt-2 text-2xl w-12 h-12 flex items-center justify-center"
--- src/util/stt.ts
+++ src/util/stt.ts
@@ -1,7 +1,38 @@
+async function gcpSTT(audio: Blob, lang: string = "en-US"): Promise<string> {
+    const key = import.meta.env.VITE_GCP_KEY;
+    const url = `https://speech.googleapis.com/v1/speech:recognize?key=${key}`
+
+    const base64Audio = await new Promise<string>((resolve) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result as string);
+        reader.readAsDataURL(audio);
+    }).then((data) => data.replace(/^data:audio\/webm;codecs=opus;base64,/, ''));
+
+
+    //value from mediarecorder(opus/webm)
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            config: {
+                encoding: 'WEBM_OPUS',
+                languageCode: lang,
+                enableAutomaticPunctuation: true,
+                model: 'latest_short'
+            },
+            audio: {content: base64Audio}
+        })
+    });
+    const data = await response.json();
+    return data?.results?.[0]?.alternatives?.[0]?.transcript;
+}
+
 export default function (setMessage?: (message: string) => void, lang: string = "en-US") {
     if (setMessage) setMessage("");
     const recognition = new ((<any>window).SpeechRecognition || (<any>window).webkitSpeechRecognition || (<any>window).mozSpeechRecognition || (<any>window).msSpeechRecognition)();
-    const ret: Promise<string> & { stop: () => any } = new Promise<string>((resolve, reject) => {
+    let mediaRecorder = new MediaRecorder(new MediaStream());
+    let tr = true;
+    const ret: Promise<string> & { stop: (force?: boolean) => any } = new Promise<string>(async (resolve, reject) => {
         let buffer = '';
         if (setMessage) setMessage('지금 말하세요...');
         recognition.lang = lang;
@@ -14,18 +45,38 @@ export default function (setMessage?: (message: string) => void, lang: string = 
             if (setMessage) setMessage(transcript);
         };
 
-        recognition.onend = () => resolve(buffer);
+        recognition.onend = () => {
+            mediaRecorder.stop();
+        }
 
         recognition.onerror = (event: any) => {
             recognition.stop();
             reject(event.error);
         }
 
+        // record audio
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true
+        });
+        mediaRecorder = new MediaRecorder(stream, {mimeType: 'audio/webm;codecs=opus'});
+        mediaRecorder.ondataavailable = (event) => {
+            if (!tr) return;
+            gcpSTT(event.data, lang).then((result) => {
+                buffer = result || buffer;
+                if (setMessage) setMessage(result);
+                resolve(result);
+            });
+            stream.getTracks().forEach((track) => track.stop());
+        }
+
         recognition.start();
+        mediaRecorder.start();
     }) as any
 
-    ret.stop = () => {
+    ret.stop = (force = false) => {
+        if (force) tr = false;
         recognition.stop();
+        mediaRecorder.stop();
     }
 
     return ret;
